a57370d6b2191e6c6267557b9ecf91f05675bd1d
amercader <amercadero@gmail.com>
2018-04-18 14:19:13 +0200

    Fixes #41, render org page trees via helpers

--- ckanext/unhcr/helpers.py
+++ ckanext/unhcr/helpers.py
@@ -2,12 +2,13 @@ from ckan import model
 from ckan.plugins import toolkit
 
 
-def render_tree():
+def render_tree(top_nodes=None):
     '''Returns HTML for a hierarchy of all data containers'''
     context = {'model': model, 'session': model.Session}
-    top_nodes = toolkit.get_action('group_tree')(
-        context,
-        data_dict={'type': 'data-container'})
+    if not top_nodes:
+        top_nodes = toolkit.get_action('group_tree')(
+            context,
+            data_dict={'type': 'data-container'})
     return _render_tree(top_nodes)
 
 
@@ -21,14 +22,17 @@ def _render_tree(top_nodes):
 def _render_tree_node(node):
     html = '<a href="/data-container/{}">{}</a>'.format(
         node['name'], node['title'])
-    if node['highlighted']:
-        html = '<strong>{}</strong>'.format(html)
     if node['children']:
         html += '<ul class="hierarchy-tree">'
         for child in node['children']:
             html += _render_tree_node(child)
         html += '</ul>'
-    html = '<li id="node_{}">{}</li>'.format(node['name'], html)
+
+    if node['highlighted']:
+        html = '<li id="node_{}" class="highlighted">{}</li>'.format(
+            node['name'], html)
+    else:
+        html = '<li id="node_{}">{}</li>'.format(node['name'], html)
     return html
 
 
